7849f1d755c193a22f77c41481a8c2856fbfe0b2
yzfu0426 <fyz04262000@gmail.com>
2022-01-06 18:56:21 -0500

    mkdir for hello

--- helloworld1.py
+++
@@ -1,9 +0,0 @@
-#First way, just use -Option, and use regular expression to replace the hyphen.
-
-import sys, re
-
-name = re.sub("\-", "", sys.argv[1])
-age = int(re.sub("\-", "", sys.argv[2]))
-
-
-print (f"In file 1, Hello my name is {name} and I am {age}")
